1902227d315a2c364a296c67c6530e3aabe2d416
strwalk <61673527+strwalk@users.noreply.github.com>
2024-03-19 15:10:59 +0900

    feat: add required label to form

--- src/app/_components/FormItem/FormItem.tsx
+++ src/app/_components/FormItem/FormItem.tsx
@@ -27,35 +27,53 @@ export function FormItem() {
       className="flex flex-col gap-8"
     >
       <section>
-        <label htmlFor="email" className="block">
-          メールアドレス
-        </label>
+        <section className="flex gap-2 items-center">
+          <label htmlFor="email" className="block">
+            メールアドレス
+          </label>
+          <span className="bg-red-500 text-white py-0.5 px-2 text-xs">
+            必須
+          </span>
+        </section>
         <input
           type="email"
           id="email"
           name="email"
+          required={true}
           className="border border-gray-300 w-full md:w-[24rem] py-1.5 px-2"
         />
       </section>
       <section>
-        <label htmlFor="username" className="block">
-          お名前
-        </label>
+        <section className="flex gap-2 items-center">
+          <label htmlFor="username" className="block">
+            お名前
+          </label>
+          <span className="bg-red-500 text-white py-0.5 px-2 text-xs">
+            必須
+          </span>
+        </section>
         <input
           type="text"
           id="username"
           name="username"
+          required={true}
           className="border border-gray-300 w-full md:w-[24rem] py-1.5 px-2"
         />
       </section>
       <section>
-        <label htmlFor="message" className="block">
-          お問い合わせ内容
-        </label>
+        <section className="flex gap-2 items-center">
+          <label htmlFor="message" className="block">
+            お問い合わせ内容
+          </label>
+          <span className="bg-red-500 text-white py-0.5 px-2 text-xs">
+            必須
+          </span>
+        </section>
         <textarea
           id="message"
           name="message"
           rows={6}
+          required={true}
           className="border border-gray-300 block w-full md:w-[24rem] py-1.5 px-2"
         />
       </section>
