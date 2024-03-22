1fec8019e80360729b1ef9eb6974fba2761f3f62
strwalk <61673527+strwalk@users.noreply.github.com>
2024-03-22 15:29:34 +0900

    fix: split the form page test into two files

--- tests/form-page/form.spec.ts
+++ tests/form-page/form.spec.ts
@@ -17,22 +17,4 @@ test.describe('Form Page', () => {
     await expect(page.getByLabel('お問い合わせ内容')).toBeVisible();
     await expect(page.getByRole('button', { name: '送信' })).toBeVisible();
   });
-
-  test('Modal', async ({ page }) => {
-    await page.getByLabel('メールアドレス').fill('test@test');
-    await page.getByLabel('お名前').fill('山田 太郎');
-    await page.getByLabel('お問い合わせ内容').fill('test');
-    await page.getByRole('button', { name: '送信' }).click();
-
-    await expect(page.getByTestId('modal')).toBeVisible();
-    await expect(
-      page.getByText('以下の内容でお問い合わせを受け付けました')
-    ).toBeVisible();
-    await expect(page.getByTestId('modal-email')).toHaveText('test@test');
-    await expect(page.getByTestId('modal-username')).toHaveText('山田 太郎');
-    await expect(page.getByTestId('modal-message')).toHaveText('test');
-
-    await page.getByRole('button', { name: '閉じる' }).click();
-    await expect(page.getByTestId('modal')).toBeHidden();
-  });
 });
---
+++ tests/form-page/modal.spec.ts
@@ -0,0 +1,25 @@
+import { test, expect } from '@playwright/test';
+
+test.beforeEach(async ({ page }) => {
+  await page.goto('/form');
+});
+
+test.describe('Form Page', () => {
+  test('Modal', async ({ page }) => {
+    await page.getByLabel('メールアドレス').fill('test@test');
+    await page.getByLabel('お名前').fill('山田 太郎');
+    await page.getByLabel('お問い合わせ内容').fill('test');
+    await page.getByRole('button', { name: '送信' }).click();
+
+    await expect(page.getByTestId('modal')).toBeVisible();
+    await expect(
+      page.getByText('以下の内容でお問い合わせを受け付けました')
+    ).toBeVisible();
+    await expect(page.getByTestId('modal-email')).toHaveText('test@test');
+    await expect(page.getByTestId('modal-username')).toHaveText('山田 太郎');
+    await expect(page.getByTestId('modal-message')).toHaveText('test');
+
+    await page.getByRole('button', { name: '閉じる' }).click();
+    await expect(page.getByTestId('modal')).toBeHidden();
+  });
+});
