3fda14004f5637f2f66f1960f694b693f82adbfc
strwalk <61673527+strwalk@users.noreply.github.com>
2024-04-28 13:11:52 +0900

    feat: add a button to return to the top page on the form page

--- src/app/form/page.tsx
+++ src/app/form/page.tsx
@@ -1,12 +1,23 @@
+import Link from 'next/link';
 import { FormItem } from '../_components/FormItem';
 
 export default function Form() {
   return (
-    <main className="py-10 px-4 md:px-10 flex justify-center items-center bg-blue-100 min-h-screen">
-      <section className="w-full md:w-[39rem] bg-white pt-16 pb-12 px-8 rounded-lg shadow-md">
-        <h1 className="text-center text-3xl">お問い合わせフォーム</h1>
-        <section className="flex justify-center mt-10 w-full">
-          <FormItem />
+    <main className="py-10 px-4 md:px-10 flex justify-center items-center bg-blue-50 min-h-screen">
+      <section>
+        <section className="w-full md:w-[39rem] bg-white pt-16 pb-12 px-8 rounded-lg shadow-md">
+          <h1 className="text-center text-3xl">お問い合わせフォーム</h1>
+          <section className="flex justify-center mt-10 w-full">
+            <FormItem />
+          </section>
+        </section>
+        <section className="flex justify-center mt-5">
+          <Link
+            href="/"
+            className="bg-blue-100 hover:bg-blue-200 py-3 px-6 rounded shadow"
+          >
+            記事一覧に戻る
+          </Link>
         </section>
       </section>
     </main>
