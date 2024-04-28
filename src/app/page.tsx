3c7ec8d0162869e22b91d250665104ef3ea8b504
strwalk <61673527+strwalk@users.noreply.github.com>
2024-04-28 13:10:42 +0900

    feat: display data obtained from cms on the top page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,54 +1,73 @@
 import { LargeArticle, NormalArticle } from './_components';
 
-export default function Home() {
+interface Articles {
+  id: string;
+  title: string;
+  contents: {
+    text: string;
+  };
+}
+
+async function getArticles() {
+  const response = await fetch(`${process.env.HYGRAPH_ENDPOINT}`, {
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer ${process.env.HYGRAPH_PERMANENT_AUTH_TOKEN}`,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      query: `
+        query Articles {
+          articles {
+            id
+            title
+            contents {
+              text
+            }
+          }
+        }
+      `,
+    }),
+  });
+
+  if (!response.ok) {
+    throw new Error('Failed to fetch Articles data');
+  }
+
+  const json = await response.json();
+  return json.data.articles;
+}
+
+export default async function Home() {
+  const articles: Articles[] = await getArticles();
+
   return (
     <main className="py-10 px-10 flex justify-center">
       <section className="w-[50rem]">
-        <h1 className="text-center text-4xl">シンプル ブログ</h1>
-        <section className="grid grid-cols-2 gap-8 mt-8">
-          <LargeArticle
-            articleId="AAA"
-            title="記事タイトルA"
-            contents="テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト"
-          />
-          <LargeArticle
-            articleId="BBB"
-            title="記事タイトルB"
-            contents="テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト"
-          />
-        </section>
-        <section className="grid grid-cols-3 gap-8 mt-6">
-          <NormalArticle
-            articleId="111"
-            title="記事タイトル1"
-            contents="テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト"
-          />
-          <NormalArticle
-            articleId="222"
-            title="記事タイトル2"
-            contents="テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト"
-          />
-          <NormalArticle
-            articleId="333"
-            title="記事タイトル3"
-            contents="テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト"
-          />
-          <NormalArticle
-            articleId="444"
-            title="記事タイトル4"
-            contents="テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト"
-          />
-          <NormalArticle
-            articleId="555"
-            title="記事タイトル5"
-            contents="テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト"
-          />
-          <NormalArticle
-            articleId="666"
-            title="記事タイトル6"
-            contents="テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト"
-          />
-        </section>
+        {articles.length > 0 ? (
+          <section className="grid grid-cols-2 gap-8 mt-8">
+            {articles.slice(0, 2).map((article) => (
+              <LargeArticle
+                key={article.id}
+                articleId={article.id}
+                title={article.title}
+                contents={article.contents.text}
+              />
+            ))}
+          </section>
+        ) : null}
+        {articles.length > 2 ? (
+          <section className="grid grid-cols-3 gap-8 mt-6">
+            {articles.slice(2).map((article) => (
+              <NormalArticle
+                key={article.id}
+                articleId={article.id}
+                title={article.title}
+                contents={article.contents.text}
+              />
+            ))}
+          </section>
+        ) : null}
       </section>
     </main>
   );
