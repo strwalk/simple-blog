8ff3d5004d2def324331e2c459f82b7f5c4b7556
strwalk <61673527+strwalk@users.noreply.github.com>
2024-04-28 13:02:12 +0900

    feat: add large article component

--- src/app/_components/LargeArticle/LargeArticle.tsx
+++ src/app/_components/LargeArticle/LargeArticle.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import Image from 'next/image';
 
 interface Props {
   articleId: string;
@@ -13,7 +14,16 @@ export function LargeArticle({ articleId, title, contents }: Props) {
       className="hover:opacity-70"
       data-test-id="large-article"
     >
-      <div className="w-full h-[12rem] bg-gradient-to-r from-slate-200 to-gray-300"></div>
+      <section className="flex justify-center">
+        <Image
+          src="/images/thumbnail.jpg"
+          width={300}
+          height={300}
+          alt={title}
+          priority={true}
+          className="h-[14rem] sm:h-[12rem] w-[28rem] sm:w-full object-cover object-[center_24%]"
+        />
+      </section>
       <h2 className="mt-4 font-bold text-lg">{title}</h2>
       <p className="text-xs mt-2 line-clamp-2 text-gray-500">{contents}</p>
     </Link>
