e917d3cb6bc44344a057a3d2ab8de091e9b08d7b
strwalk <61673527+strwalk@users.noreply.github.com>
2024-04-29 23:12:01 +0900

    fix: github workflows

--- playwright.config.ts
+++ playwright.config.ts
@@ -72,7 +72,8 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: 'yarn dev',
-    url: 'http://127.0.0.1:3000',
+    port: 3000
+    // url: 'http://127.0.0.1:3000',
     // reuseExistingServer: !process.env.CI,
   },
 });
