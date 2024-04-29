fc97f800095550aaa3f5f64a6f39d6e8d791c1cd
strwalk <61673527+strwalk@users.noreply.github.com>
2024-04-29 23:26:21 +0900

    fix: playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -72,8 +72,9 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: 'yarn dev',
-    port: 3000
-    // url: 'http://127.0.0.1:3000',
-    // reuseExistingServer: !process.env.CI,
+    url: 'http://127.0.0.1:3000',
+    reuseExistingServer: !process.env.CI,
+    stdout: 'ignore',
+    stderr: 'pipe',
   },
 });
