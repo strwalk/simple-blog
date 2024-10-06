1b89afdfa7f3adc7de0e8ad4ca733a33f6ae0e5e
strwalk <61673527+strwalk@users.noreply.github.com>
2024-10-06 17:50:46 +0900

    feat: add disable draft mode parts to header

--- src/app/_components/Header/index.tsx
+++ src/app/_components/Header/index.tsx
@@ -5,29 +5,38 @@ export function Header() {
   const { isEnabled } = draftMode();
 
   return (
-    <header>
+    <>
       {isEnabled && (
-        <div className="text-center text-base sm:text-2xl bg-orange-200 py-3 px-6">
-          これはPreview画面です
-        </div>
+        <section className="text-center bg-orange-200 py-3 px-6 flex gap-10 justify-center items-center">
+          <p className="text-base sm:text-2xl">これはPreview画面です</p>
+          <Link
+            href="/api/disable-draft"
+            prefetch={false}
+            className="py-1 px-5 bg-white rounded-md shadow hover:bg-gray-100 text-sm"
+          >
+            解除する
+          </Link>
+        </section>
       )}
-      <section className="py-3 px-8 flex justify-between items-start sm:items-center">
-        <Link href="/" className="hover:text-gray-500">
-          <h1 className="text-xl sm:text-2xl text-bold">SIMPLE BLOG</h1>
-        </Link>
-        <nav>
-          <ul>
-            <li>
-              <Link
-                href="/form"
-                className="text-orange-700 hover:text-orange-400 text-sm sm:text-lg underline underline-offset-4 decoration-orange-700"
-              >
-                CONTACT
-              </Link>
-            </li>
-          </ul>
-        </nav>
-      </section>
-    </header>
+      <header>
+        <section className="py-3 px-8 flex justify-between items-start sm:items-center">
+          <Link href="/" className="hover:text-gray-500">
+            <h1 className="text-xl sm:text-2xl text-bold">SIMPLE BLOG</h1>
+          </Link>
+          <nav>
+            <ul>
+              <li>
+                <Link
+                  href="/form"
+                  className="text-orange-700 hover:text-orange-400 text-sm sm:text-lg underline underline-offset-4 decoration-orange-700"
+                >
+                  CONTACT
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </section>
+      </header>
+    </>
   );
 }
