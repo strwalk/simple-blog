1c2884f7b1af477b02cbbd727288c48ed94bffb9
strwalk <61673527+strwalk@users.noreply.github.com>
2024-10-19 21:11:36 +0900

    fix: playwright workflow

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,8 +6,6 @@ import { defineConfig, devices } from '@playwright/test';
  */
 // require('dotenv').config();
 
-const E2E_BASE_URL = process.env.PLAYWRIGHT_BASE_URL ?? 'http://localhost:3000';
-
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
@@ -26,7 +24,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: E2E_BASE_URL,
+    baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -74,7 +72,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: 'yarn dev',
-    url: E2E_BASE_URL,
+    url: 'http://localhost:3000',
     reuseExistingServer: !process.env.CI,
     stdout: 'ignore',
     stderr: 'pipe',
