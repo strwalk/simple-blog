80050f80fbc1b534b110562a6d3afd7bd75dc277
strwalk <61673527+strwalk@users.noreply.github.com>
2024-10-21 23:50:46 +0900

    fix: error page design

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -3,7 +3,7 @@
 import { useEffect } from 'react';
 import Link from 'next/link';
 import * as Sentry from '@sentry/nextjs';
-import { Header } from './_components';
+import { Header, Footer } from './_components';
 
 export default function Error({
   error,
@@ -17,19 +17,20 @@ export default function Error({
   return (
     <main>
       <Header isPreview={false} />
-      <section className="flex justify-center h-[80vh] items-center">
-        <section className="">
+      <section className="flex justify-center h-[90vh] items-center px-10">
+        <section>
           <h2 className="text-2xl">サーバーエラーが発生しました</h2>
           <section className="mt-10 text-center">
             <Link
               href="/"
-              className="bg-orange-300 hover:bg-orange-200 py-4 px-6 rounded shadow"
+              className="bg-amber-300 hover:bg-amber-200 py-4 px-6 rounded shadow"
             >
               トップページへ戻る
             </Link>
           </section>
         </section>
       </section>
+      <Footer />
     </main>
   );
 }
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,23 +1,24 @@
 import Link from 'next/link';
-import { Header } from './_components';
+import { Header, Footer } from './_components';
 
 export default function NotFound() {
   return (
     <main>
       <Header isPreview={false} />
-      <section className="flex justify-center h-[80vh] items-center">
-        <section className="">
+      <section className="flex justify-center h-[90vh] items-center px-10">
+        <section>
           <h2 className="text-2xl">お探しのページは見つかりませんでした</h2>
           <section className="mt-10 text-center">
             <Link
               href="/"
-              className="bg-orange-300 hover:bg-orange-200 py-4 px-6 rounded shadow"
+              className="bg-amber-300 hover:bg-amber-200 py-4 px-6 rounded shadow"
             >
               トップページへ戻る
             </Link>
           </section>
         </section>
       </section>
+      <Footer />
     </main>
   );
 }
