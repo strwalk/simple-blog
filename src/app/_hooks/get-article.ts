475fa4b2bcb4b71d9a786e3c17601eae400f68c5
strwalk <61673527+strwalk@users.noreply.github.com>
2024-10-21 11:06:50 +0900

    fix: error handling of get-article

--- src/app/_hooks/get-article.ts
+++ src/app/_hooks/get-article.ts
@@ -2,18 +2,19 @@ export async function getArticle(
   articleId: string,
   isPreview: boolean
 ): Promise<ArticleType> {
-  const hygraphAuthToken = isPreview
-    ? process.env.HYGRAPH_PERMANENT_AUTH_PREVIEW_TOKEN
-    : process.env.HYGRAPH_PERMANENT_AUTH_TOKEN;
+  try {
+    const hygraphAuthToken = isPreview
+      ? process.env.HYGRAPH_PERMANENT_AUTH_PREVIEW_TOKEN
+      : process.env.HYGRAPH_PERMANENT_AUTH_TOKEN;
 
-  const response = await fetch(`${process.env.HYGRAPH_ENDPOINT}`, {
-    method: 'POST',
-    headers: {
-      Authorization: `Bearer ${hygraphAuthToken}`,
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      query: `
+    const response = await fetch(`${process.env.HYGRAPH_ENDPOINT}`, {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${hygraphAuthToken}`,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        query: `
           query Article {
             article(where: {id: "${articleId}"}) {
               id
@@ -25,13 +26,15 @@ export async function getArticle(
             }
           }
         `,
-    }),
-  });
+      }),
+    });
 
-  if (!response.ok) {
-    throw new Error('Failed to fetch Article data');
+    const json = await response.json();
+    return json.data.article;
+  } catch (err) {
+    throw new Error(
+      `[Error] Failed to fetch Article data. ArticleID: ${articleId}`,
+      { cause: err }
+    );
   }
-
-  const json = await response.json();
-  return json.data.article;
 }
