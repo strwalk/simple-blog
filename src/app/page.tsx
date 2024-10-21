d01a2bc47060ae8a172eef964f5d15091627b111
strwalk <61673527+strwalk@users.noreply.github.com>
2024-10-21 23:51:21 +0900

    fix: top page design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import { draftMode } from 'next/headers';
 import { getArticles } from './_hooks';
-import { LargeArticle, NormalArticle, Header } from './_components';
+import { LargeArticle, NormalArticle, Header, Footer } from './_components';
 
 export const dynamic = 'force-dynamic';
 
@@ -9,12 +9,12 @@ export default async function Home() {
   const articles: Articles[] = await getArticles();
 
   return (
-    <main>
+    <main className="bg-amber-100">
       <Header isPreview={isEnabled} />
-      <section className="pt-3 sm:pt-6 pb-6 sm:pb-10 px-4 sm:px-10 flex justify-center">
-        <section className="w-[50rem] sm:w-[60rem] xl:w-[70rem]">
+      <section className="pt-20 px-4 sm:px-10 flex justify-center h-full min-h-screen">
+        <section className="w-[50rem] sm:w-[60rem] xl:w-[65rem]">
           {articles.length > 0 ? (
-            <section className="grid grid-cols-1 sm:grid-cols-2 gap-8">
+            <section className="grid grid-cols-1 sm:grid-cols-2 gap-6 sm:gap-8">
               {articles.slice(0, 2).map((article) => (
                 <LargeArticle
                   key={article.id}
@@ -26,7 +26,7 @@ export default async function Home() {
             </section>
           ) : null}
           {articles.length > 2 ? (
-            <section className="grid grid-cols-1 sm:grid-cols-3 gap-8 mt-6">
+            <section className="grid grid-cols-1 sm:grid-cols-3 gap-6 sm:gap-8 mt-6">
               {articles.slice(2).map((article) => (
                 <NormalArticle
                   key={article.id}
@@ -39,6 +39,7 @@ export default async function Home() {
           ) : null}
         </section>
       </section>
+      <Footer />
     </main>
   );
 }
