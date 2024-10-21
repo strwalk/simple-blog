93f38e0a8082850ccc545d37f2ad58d1ccca1d28
strwalk <61673527+strwalk@users.noreply.github.com>
2024-10-21 23:51:06 +0900

    fix: header design

--- src/app/_components/Header/index.tsx
+++ src/app/_components/Header/index.tsx
@@ -6,9 +6,9 @@ interface Props {
 
 export function Header({ isPreview }: Props) {
   return (
-    <>
+    <section className="fixed top-0 z-10 w-full">
       {isPreview && (
-        <section className="text-center bg-orange-200 py-3 px-6 flex gap-10 justify-center items-center">
+        <section className="text-center bg-amber-200 py-3 px-6 flex gap-10 justify-center items-center">
           <p className="text-base sm:text-2xl">これはPreview画面です</p>
           <Link
             href="/api/disable-draft"
@@ -19,25 +19,29 @@ export function Header({ isPreview }: Props) {
           </Link>
         </section>
       )}
-      <header>
-        <section className="py-3 px-8 flex justify-between items-start sm:items-center">
-          <Link href="/" className="hover:text-gray-500">
-            <h1 className="text-xl sm:text-2xl text-bold">SIMPLE BLOG</h1>
-          </Link>
-          <nav>
-            <ul>
-              <li>
-                <Link
-                  href="/form"
-                  className="text-orange-700 hover:text-orange-400 text-sm sm:text-lg underline underline-offset-4 decoration-orange-700"
-                >
-                  CONTACT
-                </Link>
-              </li>
-            </ul>
-          </nav>
+      <header className="h-12 bg-white px-6 sm:px-8 shadow justify-center">
+        <section className="flex justify-center items-center h-full">
+          <section className="w-[50rem] sm:w-[64rem] xl:w-[70rem]">
+            <section className="flex justify-between items-center">
+              <Link href="/" className="hover:text-gray-500">
+                <h1 className="text-base sm:text-xl text-bold">SIMPLE BLOG</h1>
+              </Link>
+              <nav>
+                <ul>
+                  <li>
+                    <Link
+                      href="/form"
+                      className="text-sm sm:text-lg hover:text-gray-500"
+                    >
+                      CONTACT
+                    </Link>
+                  </li>
+                </ul>
+              </nav>
+            </section>
+          </section>
         </section>
       </header>
-    </>
+    </section>
   );
 }
